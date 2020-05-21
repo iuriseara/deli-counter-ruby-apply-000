157d36227ac41864f8b32a56eca5c64e67222f4e
fearless-command-2358 <iuriseara@gmail.com>
2020-05-21 16:52:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,6 @@
 #Write your code here.
+katz_deli = ["spiderman", "batman"]
+
 def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
@@ -11,12 +13,12 @@ def line(katz_deli)
 
     puts "#{message}"
   end
-
+end
 def now_serving(katz_deli)
   if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli.shift}."
+    puts "Currently serving #{katz_deli[0]}."
   end
 end
 
@@ -25,4 +27,5 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-end
+puts now_serving(katz_deli)
+puts katz_deli
